3fac910ec758fdc7ffd29e0103c7ae01b75da1f6
Ben Merchant <benmerch@gmail.com>
2024-07-11 10:06:46 -0500

    Started blog elements.

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -1,8 +1,19 @@
 
 let blogs = []
 
+
 const addBlog = (username, title, blog) => {}
 
+const createBlogElement = ({username, title, blog})=>{
+    const blogDiv = document.createElement('div');
+    const blogTitle = document.createElement('h2');
+    const blogUsername = document.createElement('p');
+    const blogBlog = document.createElement('p');
+
+    blogTitle.innerText = "Title: " + title;
+    blogUsername.innerText = "Author: " + username;
+    blogBlog.innerText = blog;
+}
 
 
 
